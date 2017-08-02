d235b6419e4a7d872ae257aa88b56e8fdff8a9b0
Jonnatas Cabral <matiasjonnatas@gmail.com>
2017-08-02 16:52:50 -0300

    TASK_3 create game report

--- reports.py
+++ reports.py
@@ -1,4 +1,6 @@
 from ORM import MongoDB
+from ORM import get_players
+from ORM import count_game_kills
 
 
 db = MongoDB(
@@ -6,3 +8,10 @@ db = MongoDB(
     collection_name='games'
 )
 db.collection.find()
+
+
+def game_summary(game):
+    return {
+        'total_kills': count_game_kills(game),
+        'players': get_players(game)
+    }
