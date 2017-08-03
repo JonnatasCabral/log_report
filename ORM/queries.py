e601ddd665c253d9775b7d8addabaae1276aad5f
Jonnatas Cabral <matiasjonnatas@gmail.com>
2017-08-03 00:05:06 -0300

    TASK_3 finalizing report of sumary game and player ranking

--- ORM/queries.py
+++ ORM/queries.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def count_game_kills(game):
     return len(game['kills'])
 
@@ -7,9 +10,9 @@ def get_players(game):
     for kill in game['kills']:
         player_killer = kill['player_killer']
         player_dead = kill['player_dead']
-        if player_killer not in players:
+        if player_killer not in players and player_killer != '<world>':
             players.append(player_killer)
-        elif player_dead not in players:
+        elif player_dead not in players and player_dead != '<world>':
             players.append(player_dead)
     return players
 
@@ -20,19 +23,28 @@ def get_players_kills(game):
 
     for kill in game['kills']:
         player_killer = kill['player_killer']
-        if player_killer not in players_kills:
+        if player_killer not in players_kills and player_killer != '<world>':
             players_kills[player_killer] = 1
-        else:
+        elif player_killer != '<world>':
             players_kills[player_killer] += 1
     return players_kills
 
 
-def get_players_deads(game):
-    players_deads = {}
+def get_players_deaths_by_world(game):
+    players_deaths = {}
     for kill in game['kills']:
+        player_killer = kill['player_killer']
         player_dead = kill['player_dead']
-        if player_dead not in players_deads:
-            players_deads[player_dead] = 1
-        else:
-            players_deads[player_dead] += 1
-    return players_deads
+        if player_killer == '<world>':
+            if player_dead not in players_deaths:
+                players_deaths[player_dead] = 1
+            else:
+                players_deaths[player_dead] += 1
+    return players_deaths
+
+
+def get_kills(game):
+    kills = Counter(get_players_kills(game))
+    deaths = Counter(get_players_deaths_by_world(game))
+    return dict(kills - deaths)
+
--- reports.py
+++ reports.py
@@ -1,17 +1,29 @@
 from ORM import MongoDB
+from ORM import get_kills
 from ORM import get_players
 from ORM import count_game_kills
+from collections import Counter
 
 
 db = MongoDB(
     database_name='quake',
     collection_name='games'
 )
-db.collection.find()
 
 
 def game_summary(game):
     return {
         'total_kills': count_game_kills(game),
-        'players': get_players(game)
+        'players': get_players(game),
+        'kills': get_kills(game)
     }
+
+
+def players_ranking():
+    ranking = Counter({})
+    games_cursor = db.collection.find()
+    for game in games_cursor:
+        ranking += Counter(get_kills(game))
+
+    ranking = dict(ranking)
+    return ranking
